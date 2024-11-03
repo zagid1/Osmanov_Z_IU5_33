907476d25409fbe1278fa64969e1e0b9ee02343c
zagid1 <osmanovzagid2005@gmail.com>
2024-11-03 19:49:23 +0300

    delete test file

--- test.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, a_, b_):
-        self.a = a_
-        self.b = b_
-
-a = Student(10, 12)
-aa = None
-print(type(aa))
-print(f"Student: {a._a} and {a._Student_b}" )
